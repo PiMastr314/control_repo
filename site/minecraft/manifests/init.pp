71feb5433ade523ca2c475a38f9e22e68c92604a
PiMastr314 <97702172+PiMastr314@users.noreply.github.com>
2022-01-21 16:46:03 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,11 +5,12 @@ class minecraft {
   file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar',
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
-  file {'opt/minecraft/eula.txt':
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
     content => 'eula=true',
   }
@@ -20,5 +21,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   }
 }
